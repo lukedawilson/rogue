66591a7e2245b2d1a26f7c7d327c7c72ba9f8fed
Luke Wilson <luke.wilson@intelligenthack.com>
2022-12-27 13:47:59 +0000

    Code tidy-up

--- Rogue/Tile.cs
+++ Rogue/Tile.cs
@@ -1,59 +1,56 @@
-using System;
+namespace Rogue;
 
-namespace Rogue
+public class Tile
 {
-  public class Tile
+  public const int Width = 8;
+  public const int Height = 8;
+
+  public Tile(string description)
   {
-    public const int Width = 8;
-    public const int Height = 8;
+    Layout = new int[Width,Height];
+    West = Array.Empty<Tuple<Tile, double>>();
+    East = Array.Empty<Tuple<Tile, double>>();
+    North = Array.Empty<Tuple<Tile, double>>();
+    South = Array.Empty<Tuple<Tile, double>>();
 
-    public Tile(string description)
+    _description = description;
+  }
+
+  public int[,] Layout { get; set; }
+
+  public Tuple<Tile, double>[] West { get; set; }
+  public Tuple<Tile, double>[] East { get; set; }
+  public Tuple<Tile, double>[] North { get; set; }
+  public Tuple<Tile, double>[] South { get; set; }
+
+  private readonly string _description;
+
+  public void Draw(int x, int y)
+  {
+    Console.SetCursorPosition(x, y);
+
+    var i = 0;
+    foreach (var p in Layout)
     {
-      Layout = new int[Width,Height];
-      West = new Tuple<Tile, double>[0];
-      East = new Tuple<Tile, double>[0];
-      North = new Tuple<Tile, double>[0];
-      South = new Tuple<Tile, double>[0];
+      Console.Write(p == 1 ? '\u2592' : ' '); // http://www.utf8-chartable.de/unicode-utf8-table.pl?start=9600&number=128
+      i++;
 
-      _description = description;
+      if (i != Width)
+        continue;
+
+      Console.CursorTop++;
+      Console.CursorLeft-=Width;
+      i = 0;
     }
+  }
 
-    public int[,] Layout { get; set; }
+  public override string ToString()
+  {
+    return _description;
+  }
 
-    public Tuple<Tile, double>[] West { get; set; }
-    public Tuple<Tile, double>[] East { get; set; }
-    public Tuple<Tile, double>[] North { get; set; }
-    public Tuple<Tile, double>[] South { get; set; }
-
-    private readonly string _description;
-
-    public void Draw(int x, int y)
-    {
-      Console.SetCursorPosition(x, y);
-
-      var i = 0;
-      foreach (var p in Layout)
-      {
-        Console.Write(p == 1 ? '\u2592' : ' '); // http://www.utf8-chartable.de/unicode-utf8-table.pl?start=9600&number=128
-        i++;
-
-        if (i != Width)
-          continue;
-
-        Console.CursorTop++;
-        Console.CursorLeft-=Width;
-        i = 0;
-      }
-    }
-
-    public override string ToString()
-    {
-      return _description;
-    }
-
-    public override int GetHashCode()
-    {
-      return _description.GetHashCode();
-    }
+  public override int GetHashCode()
+  {
+    return _description.GetHashCode();
   }
 }
